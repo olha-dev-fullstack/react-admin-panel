adff847b7da753f2bade4135155c8c99ca996d78
olha-dev-fullstack <olha.shevel.dev@gmail.com>
2025-04-17 15:56:36 +0300

    add line chart components

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,15 +1,17 @@
+import ChartBox from "../../components/chartBox/ChartBox";
 import TopBox from "../../components/topBox/TopBox";
+import { chartBoxConversion, chartBoxProduct, chartBoxRevenue, chartBoxUser } from "../../data";
 import "./home.scss";
 
 const Home = () => {
   return (
     <div className="home">
       <div className="box box1"><TopBox /></div>
-      <div className="box box2">Box</div>
-      <div className="box box3">Box</div>
-      <div className="box box4">Box</div>
-      <div className="box box5">Box</div>
-      <div className="box box6">Box</div>
+      <div className="box box2"><ChartBox {...chartBoxUser}/></div>
+      <div className="box box3"><ChartBox {...chartBoxProduct}/></div>
+      <div className="box box4"></div>
+      <div className="box box5"><ChartBox {...chartBoxConversion}/></div>
+      <div className="box box6"><ChartBox {...chartBoxRevenue}/></div>
       <div className="box box7">Box</div>
       <div className="box box8">Box</div>
       <div className="box box9">Box</div>
