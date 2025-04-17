1dc8960eab0e4eff1750f7e1543069f79be75ac3
olha-dev-fullstack <olha.shevel.dev@gmail.com>
2025-04-17 15:06:37 +0300

    configure app wrapper and add styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,47 @@
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Users from "./pages/users/Users";
+import Products from "./pages/products/Products";
+import Navbar from "./components/navbar/Navbar";
+import Menu from "./components/menu/Menu";
+import Footer from "./components/footer/Footer";
+import "./styles/global.scss";
 function App() {
+  const Layout = () => (
+    <div className="main">
+      <Navbar />
+      <div className="container">
+        <div className="menuContainer">
+          <Menu />
+        </div>
+        <div className="contentContainer">
+          {/* <QueryClientProvider client={queryClient}> */}
+          <Outlet />
+          {/* </QueryClientProvider> */}
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 
-  return (
-    <div>Hello World</div>
-  )
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "users",
+          element: <Users />,
+        },
+        {
+          path: "products",
+          element: <Products />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
