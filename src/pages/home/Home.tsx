e768c5baee8ce57f61388c9f907d19a603802cc2
olha-dev-fullstack <olha.shevel.dev@gmail.com>
2025-04-17 16:01:22 +0300

    bar charts added

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,20 +1,43 @@
+import { BarChart } from "recharts";
 import ChartBox from "../../components/chartBox/ChartBox";
 import TopBox from "../../components/topBox/TopBox";
-import { chartBoxConversion, chartBoxProduct, chartBoxRevenue, chartBoxUser } from "../../data";
+import {
+  barChartBoxRevenue,
+  barChartBoxVisit,
+  chartBoxConversion,
+  chartBoxProduct,
+  chartBoxRevenue,
+  chartBoxUser,
+} from "../../data";
 import "./home.scss";
+import BarChartBox from "../../components/barChartBox/BarChartBox";
 
 const Home = () => {
   return (
     <div className="home">
-      <div className="box box1"><TopBox /></div>
-      <div className="box box2"><ChartBox {...chartBoxUser}/></div>
-      <div className="box box3"><ChartBox {...chartBoxProduct}/></div>
+      <div className="box box1">
+        <TopBox />
+      </div>
+      <div className="box box2">
+        <ChartBox {...chartBoxUser} />
+      </div>
+      <div className="box box3">
+        <ChartBox {...chartBoxProduct} />
+      </div>
       <div className="box box4"></div>
-      <div className="box box5"><ChartBox {...chartBoxConversion}/></div>
-      <div className="box box6"><ChartBox {...chartBoxRevenue}/></div>
+      <div className="box box5">
+        <ChartBox {...chartBoxConversion} />
+      </div>
+      <div className="box box6">
+        <ChartBox {...chartBoxRevenue} />
+      </div>
       <div className="box box7">Box</div>
-      <div className="box box8">Box</div>
-      <div className="box box9">Box</div>
+      <div className="box box8">
+        <BarChartBox {...barChartBoxVisit} />
+      </div>
+      <div className="box box9">
+        <BarChartBox {...barChartBoxRevenue} />
+      </div>
     </div>
   );
 };
